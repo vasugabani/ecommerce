b3dd0b3d48e57bb8820d1d9dc6bc99f86d7dfa5f
vansh1399 <gabanivansh49@gmail.com>
2024-05-27 18:36:01 +0530

    27-5-init

---
+++ App.js
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+import React from 'react'
+import Signup from './app/container/Signup/Signup';
+import Login from './app/container/Login/Login';
+
+export default function App() {
+  return (
+    <>
+    {/* <Signup/> */}
+    <Login/>
+    </>
+//     <View style={{ backgroundColor: '#F9F9F9' }}>
+//       <Text style={{ padding: 10 }}><FontAwesome name='angle-left' size={50} color='#222222' /></Text>
+//       <Text style={{ fontFamily: 'Metropolis-Bold', padding: 10, fontSize: 25, color: '#222222' }}>Sign up</Text>
+//       <TextInput
+//         style={styles.input}
+//         placeholder='Name'
+//         autoCapitalize="none"
+//         placeholderTextColor='#9B9B9B'
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder='Email'
+//         secureTextEntry={true}
+//         autoCapitalize="none"
+//         placeholderTextColor='#9B9B9B'
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder='Password'
+//         autoCapitalize="none"
+//         placeholderTextColor='#9B9B9B'
+//       />
+//       <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', padding: 10, alignItems: 'center' }}>
+//         <Text style={{ color: '#222222' }}>Already have an account</Text>
+//         <FontAwesome name='long-arrow-right' size={30} color='#DB3022' /></View>
+//     </View>
+  )
+}
+
+// // const styles = StyleSheet.create({
+// //   input: {
+// //     width: 350,
+// //     height: 55,
+// //     backgroundColor: '#FFFFFF',
+// //     margin: 10,
+// //     padding: 8,
+// //     color: 'black',
+// //     borderRadius: 14,
+// //     fontSize: 18,
+// //     fontWeight: '500',
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   }
+// })
---
+++ app/container/Login/Login.js
@@ -0,0 +1,88 @@
+import { View, Text, StatusBar, StyleSheet,TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+export default function Login() {
+    return (
+        <View style={Styles.container}>
+            <StatusBar
+                backgroundColor="#fff"
+                barStyle="dark-content"
+            />
+            <View style={{ marginTop: 20 }}>
+                <EvilIcons name="chevron-left" size={45} color="black" />
+            </View>
+            <Text style={{fontFamily:'Metropolis-Bold',fontSize:40,color:'#222222',margin:20}}>
+                Login
+            </Text>
+            <TextInput
+                style={Styles.input}
+                placeholder='Email'
+            />
+               <TextInput
+                style={Styles.input}
+                placeholder='Password'
+            />
+            <Text style={Styles.already}>
+                Already have an account?
+            </Text>
+            <TouchableOpacity style={{alignItems:'center'}}>
+                <Text style={Styles.Sign}>Sign up</Text>
+            </TouchableOpacity>
+            <View>
+                <Text style={Styles.social}>
+                    Forgot your Password
+                </Text>
+            </View>
+            <View style={Styles.mainview}>
+                <TouchableOpacity style={Styles.facebook}><FontAwesome  name="facebook-square" size={45} color="#23527C"/></TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const Styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#F4F4F4'
+    },
+    input:{
+        height: 40,
+        margin: 20,
+        borderWidth: 0,
+        padding: 10,
+        borderRadius:5,
+        backgroundColor:'#FFFFFF',
+    },
+    already:{
+        color:'#222222',
+        textAlign:'center'
+    },
+    Sign:{
+        width:'80%',
+        height:50,
+        backgroundColor:'#DB3022',
+        borderRadius:50,
+        textAlign:'center',
+        padding:10,
+        fontFamily:'Metropolis-ExtraBold',
+        color:'#FFFFFF',
+        fontSize:18,
+        marginTop:30,
+    },
+    social:{
+        textAlign:'center',
+        marginVertical:80,
+        color:'#222222'
+    },
+    facebook:{
+       paddingHorizontal:13,
+    },
+    mainview:{
+        flexDirection:'row',
+        justifyContent:'center',
+        marginBottom:20
+    }
+
+})
